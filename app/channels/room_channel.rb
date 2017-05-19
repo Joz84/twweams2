c25d3ca2117637fa08c3e8bdfba079f794c23f05
Charly MARTIN <charly.martin.1207@gmail.com>
2017-05-19 16:21:34 +0200

    room_channel

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,12 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak
+  end
+end
